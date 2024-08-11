2d52ddeaadb923063aabecaa7a9aa5e78aafa1c8
Abdelmathin Habachi <abdelmathinhabachi@gmail.com>
2024-08-11 10:19:49 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cloud1
 This project is an introduction to cloud servers
+
+https://us-west-2.console.aws.amazon.com/ec2/home?region=us-west-2#Instances:instanceState=running
--- deployment/scripts/run.sh
+++ deployment/scripts/run.sh
@@ -8,8 +8,13 @@ apt-get install -y docker-compose
 apt-get install -y git
 apt-get install -y python3
 
+git clone https://github.com/Abdelmathin/inception inception
+cd inception && make
 
-mkdir test
-cd test
-echo '<h1>Hello, Cloud-1!</h1>' > index.html
+
+mkdir -p inception
+cd inception
+# echo "" > srcs/.env
+
+echo "<h1>Hello, {var.env_example_key}!</h1>" > index.html
 sudo python3 -m http.server 80
