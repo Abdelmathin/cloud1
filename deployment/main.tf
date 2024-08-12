3d9af28cecd2b5b3df312ddbb36abeb419f5abe9
Abdelmathin Habachi <abdelmathinhabachi@gmail.com>
2024-08-12 06:34:09 +0100

    auto-commit

--- deployment/main.tf
+++ deployment/main.tf
@@ -64,6 +64,13 @@ resource "aws_security_group" "web" {
     }
 
     ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
--- deployment/scripts/run.sh
+++ deployment/scripts/run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-cd
+
 apt-get update
 apt-get install -y docker.io
-systemctl start docker
-systemctl enable docker
 apt-get install -y docker-compose
 apt-get install -y git
 apt-get install -y python3
 
-# git clone https://github.com/Abdelmathin/inception inception
-# echo "" > srcs/.env
-# cd inception && make
+systemctl start docker
+systemctl enable docker
 
-mkdir -p /tmp/web443example && echo '<h1>Hello Cloud-1, 443 PORT</h1>' > /tmp/web443example/index.html && sudo python3 -m http.server 443 --directory /tmp/web443example --bind 0.0.0.0 &
-mkdir -p /tmp/web80example  && echo '<h1>Hello Cloud-1, 80 PORT</h1>'  > /tmp/web80example/index.html  && sudo python3 -m http.server  80 --directory /tmp/web80example  --bind 0.0.0.0 &
+CLOUD1_REPOSITORY_PATH="$(cd "$(dirname "$0")/../.." && pwd)"
+INCEPTION_REPOSITORY_PATH="${CLOUD1_REPOSITORY_PATH}/inception"
+
+[ ! -d "${INCEPTION_REPOSITORY_PATH}" ] && cd "${CLOUD1_REPOSITORY_PATH}" && git clone "https://github.com/Abdelmathin/inception" "inception"
+
+cd "${INCEPTION_REPOSITORY_PATH}"
+mkdir -p srcs && env > srcs/.env
+
+python3 -m http.server  80 --directory . --bind 0.0.0.0 &
+python3 -m http.server 443 --directory . --bind 0.0.0.0 &
