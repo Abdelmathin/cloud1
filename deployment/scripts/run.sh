080ce740fce0a199063f1f238a918aa004f5471e
Abdelmathin Habachi <abdelmathinhabachi@gmail.com>
2024-08-12 06:37:46 +0100

    auto-commit

--- deployment/scripts/run.sh
+++ deployment/scripts/run.sh
@@ -9,13 +9,17 @@ apt-get install -y python3
 systemctl start docker
 systemctl enable docker
 
+if [ -z "${INCEPTION_REPOSITORY_URL}" ]; then
+    INCEPTION_REPOSITORY_URL="https://github.com/Abdelmathin/inception"
+fi
+
 CLOUD1_REPOSITORY_PATH="$(cd "$(dirname "$0")/../.." && pwd)"
 INCEPTION_REPOSITORY_PATH="${CLOUD1_REPOSITORY_PATH}/inception"
 
-[ ! -d "${INCEPTION_REPOSITORY_PATH}" ] && cd "${CLOUD1_REPOSITORY_PATH}" && git clone "https://github.com/Abdelmathin/inception" "inception"
+[ ! -d "${INCEPTION_REPOSITORY_PATH}" ] && cd "${CLOUD1_REPOSITORY_PATH}" && git clone "${INCEPTION_REPOSITORY_URL}" "inception"
 
 cd "${INCEPTION_REPOSITORY_PATH}"
-mkdir -p srcs && env > srcs/.env
+[ -d "srcs" ] && env > srcs/.env
 
 python3 -m http.server  80 --directory . --bind 0.0.0.0 &
 python3 -m http.server 443 --directory . --bind 0.0.0.0 &
