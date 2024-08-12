fa231f4a8f8b8eeb8c94941c57dc6cae7818e90a
Abdelmathin Habachi <abdelmathinhabachi@gmail.com>
2024-08-12 04:04:47 +0100

    update

--- deployment/scripts/run.sh
+++ deployment/scripts/run.sh
@@ -8,13 +8,9 @@ apt-get install -y docker-compose
 apt-get install -y git
 apt-get install -y python3
 
-git clone https://github.com/Abdelmathin/inception inception
-cd inception && make
-
-
-mkdir -p inception
-cd inception
+# git clone https://github.com/Abdelmathin/inception inception
 # echo "" > srcs/.env
+# cd inception && make
 
-echo "<h1>Hello, {var.env_example_key}!</h1>" > index.html
-sudo python3 -m http.server 80
+mkdir -p /tmp/web443example && echo '<h1>Hello Cloud-1, 443 PORT</h1>' > /tmp/web443example/index.html && sudo python3 -m http.server 443 --directory /tmp/web443example --bind 0.0.0.0 &
+mkdir -p /tmp/web80example  && echo '<h1>Hello Cloud-1, 80 PORT</h1>'  > /tmp/web80example/index.html  && sudo python3 -m http.server  80 --directory /tmp/web80example  --bind 0.0.0.0 &
